9f08cf30393438a4a0f16e6ff99710283d65f1a0
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2023-07-31 23:01:52 -0500

    CHG reading data.json

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import data from '../../data/data.json';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  jobs = data;
 
+  constructor() {
+    console.log('data', this.jobs);
+  }
 }
